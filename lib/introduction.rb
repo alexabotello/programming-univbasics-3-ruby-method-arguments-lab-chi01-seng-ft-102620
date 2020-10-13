3e0e471f0c53a45f2d8cd4f02f45c0ef7968889f
Alexa Botello <abotello20@yahoo.com>
2020-10-13 04:29:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language_optional(name, language = "Ruby")
 end 
 
 def introduction_with_language_optional(name, language = "Python")
-  puts "Hi, my name is #{name} and I am learning to program in Python."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 
